395fe24310ffe759d08fa15cf804251e6b8474a3
Esan <msdcf101@msn.com>
2022-12-20 14:32:05 +0100

    minor changes

--- PythonLib.py
+++ PythonLib.py
@@ -16,12 +16,18 @@ from tkinter import filedialog
 
 import matplotlib.pyplot as plt
 import mplcursors as mpl
+from mpl_axes_aligner import align
 
 import ruptures as rpt
+
+plt.rc('font', size= 20)
+
+
 
 #%%
 def data_transfer_force():
     filename_force = filedialog.askopenfilename(initialdir='Z:/ThesesMaster/Sundaralingam Esan/050__Messdaten/Messungen/', 
+                                                filetypes = (('txt files', '*.txt'), ('all files', '*.*')),
                                                 title='File Open Kraftmessung; Bitte die .txt Datei auswählen')
     data_force = pd.read_csv(filename_force,
                              sep=';',
@@ -34,6 +40,7 @@ def data_transfer_force():
 
 def data_transfer_smu():
     filename_res = filedialog.askopenfilename(initialdir='Z:/ThesesMaster/Sundaralingam Esan/050__Messdaten/Messungen/',  
+                                              filetypes = (('csv files', '*.csv'), ('all files', '*.*')),
                                               title='File Open Widerstandsmessung; Bitte die .csv Datei auswählen')
     data_smu = pd.read_csv(filename_res,
                            sep=',',
@@ -44,12 +51,24 @@ def data_transfer_smu():
 
 def data_transfer_ref():
     filename_ref = filedialog.askopenfilename(initialdir='Z:/ThesesMaster/Sundaralingam Esan/050__Messdaten/Messungen/', 
+                                              filetypes = (('txt files', '*.txt'), ('all files', '*.*')),
                                               title='File Open Referenzmessung; Bitte die .txt Datei auswählen')  
     data_ref = pd.read_csv(filename_ref,
                            sep=';',
                            dtype=float)
     
     return data_ref
+
+def data_transfer_bible():
+    filename = filedialog.askopenfilename(initialdir='Z:/ThesesMaster/Sundaralingam Esan/050__Messdaten/Messungen/',
+                                          filetypes = (('txt files', '*.txt'), ('all files', '*.*')),
+                                          title= 'File Open Bible')
+    
+    data = pd.read_csv(filename,
+                       sep=',',
+                       dtype=float)
+    
+    return data
     
 #%%
 def export_smu_t(data_smu):
@@ -84,61 +103,311 @@ def export_force_s(data_force):
     return s
 
 #%%
-def compute_time_diff_one(t_r, r, tensile):
+def compute_time_diff_res(time, data):        
+    r_time_temp = time[time <= 34]
+    r_time = r_time_temp
+    r_time = r_time[r_time_temp >= 27.5]
+    r_value = data[time <= 34]
+    r_value = r_value[r_time_temp >= 27.5]
     
-    if tensile == True:
-        r_time = t_r[t_r <= 50]
-        r_time = r_time[100:len(r_time)]
-        r_value = r[t_r <=50]
-        r_value = r_value[100:len(r_value)]
-    elif tensile == False:
-        r_time = t_r[t_r <= 31]
-        r_time = r_time[100:len(r_time)]
-        r_value = r[t_r <= 31]
-        r_value = r_value[100:len(r_value)]
+    r_mean_time_temp = time[time <= 29.5]
+    r_mean_time = r_mean_time_temp
+    r_mean_time = r_mean_time[r_mean_time_temp >= 27.5]
+    r_mean_value = data[time <= 29.5]
+    r_mean_value = r_mean_value[r_mean_time_temp >= 27.5]
         
-    b, a = sg.butter(3, 0.05, 'lowpass')
+    b, a = sg.butter(3, 0.1, 'lowpass')
     filtered_dr = sg.filtfilt(b, a, np.gradient(r_value, r_time))
     
-    result = rpt.Pelt(model='l2').fit_predict(filtered_dr, 0.0015)
+    result_temp = rpt.Window(width=10 ,model='ar', jump=1).fit_predict(filtered_dr, 1)
     
-    rpt.display(filtered_dr, result)
+    r_min = min(r_value)
+    r_mean = np.mean(r_mean_value)
+        
+    for i in range(len(r_value)):
+        if r_value[i] == r_min:
+            result_temp1 = i
+            
+    for i in range(len(r_value)):
+        if r_value[i] > r_mean + abs(r_mean)*0.05 or r_value[i] > r_mean - abs(r_mean)*0.05:
+            result_temp2 = i
+            break
+        
+    # if result_temp1 < 10:
+    #     result = result_temp2
+    # else:
+    #     result = result_temp1
+    
+    result = result_temp2
+    
+    rpt.display(r_value, result_temp, [result])
     
     return result
 
-def compute_time_diff(t_F, F, t_r, r, tensile):
-    result = compute_time_diff_one(t_r, r, tensile)
-    result2 = compute_time_diff_one(t_F, F, tensile)
+def compute_time_diff_force(time, data):
+    r_time_temp = time[time <= 34]
+    r_time = r_time_temp
+    r_time = r_time[r_time_temp >= 27.5]
+    r_value = data[time <= 34]
+    r_value = r_value[r_time_temp >= 27.5]
+        
+    b, a = sg.butter(3, 0.1, 'lowpass')
+    filtered_dr = sg.filtfilt(b, a, np.gradient(r_value, r_time))
     
-    r_time = t_r[t_r <= 50]
-    r_time = r_time[100:len(r_time)]
-    r_value = r[t_r <=50]
-    r_value = r_value[100:len(r_value)]
+    result_temp = rpt.BottomUp(model='ar', jump=1).fit_predict(filtered_dr, 1)
     
-    f_time = t_F[t_F <= 50]
-    f_time = f_time[100:len(f_time)]
-    f_value = F[t_F <= 50]
-    f_value = f_value[100:len(f_value)]
+    for i in range(len(r_time)):
+        if r_time[i] == 30.4:
+            result = i
+            break
     
-    diff = f_time[result2[0]] - r_time[result[0]]
+    rpt.display(r_value, result_temp, [result])
     
-    return diff, f_time[result2[0]]
+    return result
 
-def compute_gradient(t_F, F, t_r, r):
+def compute_time_diff(t_F, F, t_r, r):
+    result = compute_time_diff_res(t_r, r)
+    result2 = compute_time_diff_force(t_F, F)
+
+    r_time_temp = t_r[t_r <= 34]
+    r_time = r_time_temp
+    r_time = r_time[r_time_temp >= 27.5]
+    r_value = r[t_r <= 34]
+    r_value = r_value[r_time_temp >= 27.5]
     
-    r_max = []
-    for i in range(len(r)):
-        if r[i] == max(r):
-            r_max.append(i)
+    f_time_temp = t_F[t_F <= 34]
+    f_time = f_time_temp
+    f_time = f_time[f_time_temp >= 27.5]
+    f_value = F[t_F <= 34]
+    f_value = f_value[f_time_temp >= 27.5]    
+
+    diff = f_time[result2] - r_time[result]
     
-    b, a = sg.butter(3, 0.05, 'lowpass')
-    filtered_dr = sg.filtfilt(b, a, np.gradient(r[:r_max[0]-1], t_r[:r_max[0]-1]))
+    return [diff, f_time[result2]]
+
+#%%
+def compute_strain_res(l_0, t_r, R, t_f, F, s):
+    strain = s/l_0 * 100
+    resampled_R = np.interp(t_f, t_r, R)
     
-    b, a = sg.butter(3, 0.01, 'lowpass')
-    filtered_dF = sg.filtfilt(b, a, np.gradient(F, t_F))
+    for i in range(len(resampled_R)):
+        if resampled_R[i] > 1.0e+6:
+            index = i-1
+            break
+        # elif resampled_R[i] == max(resampled_R):
+        #     index = i
+        else:
+            index = i
     
-    return [[filtered_dF], [filtered_dr]]
+    resampled_R = resampled_R[0:index]
+    strain = strain[0:index]
+    F = F[0:index]
     
+    reg_R = 0
+    reg_F = 0
+    
+    return [strain, F, resampled_R, reg_F, reg_R]
+
+def compute_res_max_strain(strain, F, R):
+    
+    strain_temp = strain[strain <= 1.9]
+    x_strain = strain_temp
+    x_strain = x_strain[strain_temp >= 1]
+    res = R[strain <= 1.9]
+    res = res[strain_temp >= 1]
+    
+    for i in range(len(res)):
+        if res[i] == max(res):
+            x_max = x_strain[i]
+            y_max = res[i]
+            
+    for i in range(len(strain)):
+        if strain[i] == x_max:
+            temp_1 = strain[0:i]
+            temp_2 = F[0:i]
+            temp_3 = R[0:i]
+            break
+    
+    return [x_max, y_max, temp_1, temp_2, temp_3]
+
+def compute_strain_res_rmax(strain, F, resampled_R):
+    
+    strain_05_temp = strain
+    resampled_R_05 = resampled_R
+    F_05 = F
+    
+    reg_R_05 = st.linregress(strain_05_temp, resampled_R_05)
+    # for i in range(len(strain_05_temp)):
+    #     reg_R_05 = st.linregress(strain_05_temp[i:len(strain_05_temp)],resampled_R_05[i:len(strain_05_temp)])
+    #     if reg_R_05.rvalue**2 >= 0.995:
+    #         break
+    reg_F_05 = st.linregress(strain_05_temp, F_05)
+    
+    return [strain_05_temp, F_05, resampled_R_05, reg_F_05, reg_R_05]
+
+def compute_strain_res_100(strain, F, resampled_R):
+    
+    limit = 20
+    
+    resampled_R_temp = resampled_R[resampled_R < limit]
+    F = F[resampled_R < limit]
+    strain = strain[resampled_R < limit]
+    
+    reg_F = 0
+    reg_R = 0
+    
+    return [strain, F, resampled_R_temp, reg_F, reg_R]
+
+#%%
+def plot(t, R):
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Zeit [s]')
+    ax.set_ylabel('Widerstand [Ohm]')
+    ax.plot(t, R, label= 'Widerstandsänderung')
+    ax.legend()
+    mpl.cursor(hover=True)
+
+
+def plot_force(t_F, F, s, t_F_ref, F_ref, s_ref):
+    fig, ax = plt.subplot_mosaic([[0,2],[1,2]])
+    fig.set_tight_layout(True)
+    
+    ax[0].set_xlabel('Zeit [s]')
+    ax[0].set_ylabel('Kraft [N]', color= 'tab:red')
+    ax[0].plot(t_F, F, label= 'Kraftmessung')
+    ax[0].plot(t_F_ref, F_ref, color='black', label= 'Referenz')
+    ax[0].legend()
+    
+    ax[1].set_xlabel('Zeit [s]')
+    ax[1].set_ylabel('Weg [mm]')
+    ax[1].plot(t_F, s, label= 'Wegmessung')
+    ax[1].plot(t_F_ref, s_ref, color='black', label= 'Referenz')
+    ax[1].legend()
+    
+    ax[2].set_xlabel('Weg [mm]')
+    ax[2].set_ylabel('Kraft [N]')
+    ax[2].plot(s, F, label= 'Messung')
+    ax[2].plot(s_ref, F_ref, color='black', label= 'Referenz')
+    ax[2].legend()
+    
+def plot_res_and_force(t_r, r_d, t_F, F, s, t_F_ref, F_ref, s_ref, y_achse, res_label, ref_label): 
+    fig, ax = plt.subplot_mosaic([[0],[1]])
+    fig.set_tight_layout(True)
+    
+    ax[0].set_xlabel('Zeit [s]')
+    ax[0].set_ylabel('Kraft [N]', color = 'tab:red')
+    ax[0].tick_params(axis= 'y', labelcolor= 'tab:red')
+    ax[0].set_ylim(bottom = -max(F)*0.1 , top= max(F) + max(F)*0.01)
+    ax[0].plot(t_F, F, color= 'tab:red', label= 'Kraft Prüfmaschine')
+    ax[0].legend(bbox_to_anchor =(0,1.1), loc= 'upper left')
+    
+    ax2 = ax[0].twinx()
+    ax2.set_ylabel(y_achse,
+                   color= 'tab:blue')
+    ax2.tick_params(axis= 'y', labelcolor= 'tab:blue')
+    ax2.plot(t_r, r_d, color= 'tab:blue', label= res_label)
+    ax2.legend(bbox_to_anchor =(1,1.1), loc= 'upper right')
+    
+    align.yaxes(ax[0], 0, ax2, min(r_d))
+    
+    ax[1].set_xlabel('Zeit [s]')
+    ax[1].set_ylabel('Weg [mm]', color = 'tab:red')
+    ax[1].tick_params(axis= 'y', labelcolor= 'tab:red')
+    ax[1].plot(t_F, s, color= 'tab:red', label= 'Weg Prüfmaschine')
+    ax[1].legend(bbox_to_anchor =(0,1.1), loc= 'upper left')
+
+    ax3 = ax[1].twinx()
+    ax3.set_ylabel(y_achse,
+                   color= 'tab:blue')
+    ax3.tick_params(axis= 'y', labelcolor= 'tab:blue')
+    ax3.plot(t_r, r_d, color= 'tab:blue', label= res_label)
+    ax3.legend(bbox_to_anchor =(1,1.1), loc= 'upper right')
+    
+    align.yaxes(ax[1], 0, ax3, min(r_d))
+    
+    fig, ax = plt.subplots()
+    ax.set_xlabel('Weg [mm]')
+    ax.set_ylabel('Kraft [N]')
+    ax.plot(s, F, label= 'Messung', color= 'tab:red')
+    ax.plot(s_ref, F_ref, label= 'Referenz: '+ ref_label, color= 'black')
+    ax.legend()
+    
+    mpl.cursor(hover=True)
+
+def plot_strain_res(strain, F, resampled_r, reg_F, reg_R):
+    fig, ax = plt.subplots()
+    fig.set_tight_layout(True)
+    
+    ax.set_xlabel(r'$ \varepsilon (Strain) $ [%]')
+    ax.set_ylabel('Kraft [N]')
+    ax.plot(strain, F, label= 'Dehnung - Kraft', color= 'tab:red')
+    if isinstance(reg_F, st._stats_mstats_common.LinregressResult):
+       ax.plot(strain, reg_F.intercept + reg_F.slope * strain, 
+               '--r', label= 'Lin-fit-F R^2= ' + str(round(reg_F.rvalue**2,6)))
+    ax.legend(bbox_to_anchor =(0,1.1), loc= 'upper left')
+    
+    ax2 = ax.twinx()
+    ax2.set_ylabel(r'$\Delta R/R_0 $ [%]', color= 'tab:blue')
+    ax2.tick_params(axis= 'y', labelcolor= 'tab:blue')
+    # ax2.set_ylim(bottom = -max(resampled_r)*0.1, top = max(resampled_r) + max(resampled_r)*0.1)
+    ax2.plot(strain, resampled_r, label= 'Dehnung - Widerstand', color= 'tab:blue')
+    if isinstance(reg_R, st._stats_mstats_common.LinregressResult):
+       ax2.plot(strain, reg_R.intercept + reg_R.slope * strain, 
+               '--b', label= 'Lin-fit-R R^2= ' + str(round(reg_R.rvalue**2,6)))
+    ax2.legend(bbox_to_anchor =(1,1.1), loc= 'upper right')
+    
+    align.yaxes(ax, 0.0, ax2, min(resampled_r))
+    mpl.cursor(hover=True)
+
+def plot_bible(data, data_mean):
+    fig, ax = plt.subplots()
+    fig.set_tight_layout(True)
+    
+    ax.set_xlabel('Dicke [mm]')
+    ax.set_ylabel(r'$ \varepsilon (Strain) $ [%]', color= 'tab:red')
+    ax.scatter(data['Dicke'], data['Dehnung'], label= 'Dehnung-Dicke', color='tab:red')
+    ax.errorbar(data_mean['Dicke'], 
+                data_mean['MeanDehnung'], 
+                yerr=data_mean['STDDehnung'], label= 'Mean Dehnung-Dicke', ls='--', color='r')
+    ax.legend(bbox_to_anchor =(0,1.1), loc= 'upper left')
+    
+    ax2 = ax.twinx()
+    ax2.set_ylabel(r'$\Delta R/R_0 $ [%]', color= 'tab:blue')
+    ax2.scatter(data['Dicke'], data['Widerstandsänderung'], label= 'Widerstandsänderung-Dicke', color='tab:blue')
+    ax2.errorbar(data_mean['Dicke'], 
+                 data_mean['MeanWiderstand'], 
+                 yerr=data_mean['STDWiderstand'], label= 'Mean Widerstandsänderung-Dicke', ls='--', color='b')
+    ax2.legend(bbox_to_anchor =(1,1.1), loc= 'upper right')
+    
+    align.yaxes(ax, min(data['Dehnung']), ax2, min(data['Widerstandsänderung']))
+    # mpl.cursor(hover=True)
+    
+    fig2, ax = plt.subplots()
+    fig2.set_tight_layout(True)
+    
+    ax.set_xlabel('Dicke [mm]')
+    ax.set_ylabel(r'$ \varepsilon (Strain) $ [%]', color= 'tab:red')
+    ax.set_xlim(left=0.29, right=1.1)
+    ax.scatter(data['Dicke'], data['Dehnung'], label= 'Dehnung-Dicke', color='tab:red')
+    ax.errorbar(data_mean['Dicke'], 
+                data_mean['MeanDehnung'], 
+                yerr=data_mean['STDDehnung'], label= 'Mean Dehnung-Dicke', ls='--', color='r')
+    ax.legend(bbox_to_anchor =(0,1.1), loc= 'upper left')
+    
+    fig3, ax2 = plt.subplots()
+    fig3.set_tight_layout(True)
+    ax2.set_xlabel('Dicke [mm]')
+    ax2.set_ylabel(r'$\Delta R/R_0 $ [%]', color= 'tab:blue')
+    ax2.set_xlim(left=0.29, right=1.1)
+    ax2.scatter(data['Dicke'], data['Widerstandsänderung'], label= 'Widerstandsänderung-Dicke', color='tab:blue')
+    ax2.errorbar(data_mean['Dicke'], 
+                  data_mean['MeanWiderstand'], 
+                  yerr=data_mean['STDWiderstand'], label= 'Mean Widerstandsänderung-Dicke', ls='--', color='b')
+    ax2.legend(bbox_to_anchor =(1,1.1), loc= 'upper right')
+    
+    align.yaxes(ax, min(data['Dehnung']), ax2, min(data['Widerstandsänderung']))
+    # mpl.cursor(hover=True)
+#%%
 def compute_force_lin_err(t_F, F, s, bkps):
     b,a = sg.butter(3, 0.01, 'lowpass')
     filtered_dF = sg.filtfilt(b, a, np.gradient(F, t_F))
@@ -201,112 +470,10 @@ def compute_res_lin_err(data_smu, t_r, R, t_F, F, bkps):
     
     return [[x_f], [r_value], [lin_fit], [lin_err]]
 
-def compute_strain_res(l_0, A_0, t_r, R, s, F, data, tensile):
-    stress = F / A_0
-    strain = s/l_0 * 100
-    sg.resample(R, len(s))
-    result = compute_time_diff_one(data, t_r, R, tensile)
-    
-
-#%%
-
-def plot_force(t_F, F, s, t_F_ref, F_ref, s_ref):
-    fig, ax = plt.subplot_mosaic([[0,2],[1,2]])
-    fig.set_tight_layout(True)
-    
-    ax[0].set_xlabel('Zeit [s]')
-    ax[0].set_ylabel('Kraft [N]', color= 'tab:red')
-    ax[0].plot(t_F, F, label= 'Kraftmessung')
-    ax[0].plot(t_F_ref, F_ref, color='black', label= 'Referenz')
-    ax[0].legend()
-    
-    ax[1].set_xlabel('Zeit [s]')
-    ax[1].set_ylabel('Weg [mm]')
-    ax[1].plot(t_F, s, label= 'Wegmessung')
-    ax[1].plot(t_F_ref, s_ref, color='black', label= 'Referenz')
-    ax[1].legend()
-    
-    ax[2].set_xlabel('Weg [mm]')
-    ax[2].set_ylabel('Kraft [N]')
-    ax[2].plot(s, F, label= 'Messung')
-    ax[2].plot(s_ref, F_ref, color='black', label= 'Referenz')
-    ax[2].legend()
-    
-    plt.show()
-    
-def plot_res_and_force(t_r, r_d, t_F, F, s, t_F_ref, F_ref, s_ref, y_achse, res_label, ref_label, sensor): 
-    
-    r_max = []
-    for i in range(len(r_d)):
-        if r_d[i] == max(r_d):
-            r_max.append(i)
-    
-    fig, ax = plt.subplot_mosaic([[0],[1]])
-    fig.set_tight_layout(True)
-    ax[0].set_xlabel('Zeit [s]')
-    ax[0].set_ylabel('Kraft [N]', color = 'tab:red')
-    ax[0].tick_params(axis= 'y', labelcolor= 'tab:red')
-    ax[0].plot(t_F, F, color= 'tab:red', label= 'Kraft Prüfmaschine')
-    ax[0].legend(bbox_to_anchor =(0,1.1), loc= 'upper left')
-    
-    ax2 = ax[0].twinx()
-    ax2.set_ylabel(y_achse,
-                   color= 'tab:blue')
-    ax2.tick_params(axis= 'y', labelcolor= 'tab:blue')
-    ax2.set_ylim(None, r_d[r_max[0]-1], auto= sensor)
-    ax2.plot(t_r, r_d, color= 'tab:blue', label= res_label)
-    ax2.legend(bbox_to_anchor =(1,1.1), loc= 'upper right')
-    
-    ax[1].set_xlabel('Zeit [s]')
-    ax[1].set_ylabel('Weg [mm]', color = 'tab:red')
-    ax[1].tick_params(axis= 'y', labelcolor= 'tab:red')
-    ax[1].plot(t_F, s, color= 'tab:red', label= 'Weg Prüfmaschine')
-    ax[1].legend(bbox_to_anchor =(0,1.1), loc= 'upper left')
-
-    ax3 = ax[1].twinx()
-    ax3.set_ylabel(y_achse,
-                   color= 'tab:blue')
-    ax3.tick_params(axis= 'y', labelcolor= 'tab:blue')
-    ax3.set_ylim(None, r_d[r_max[0]-1], auto= sensor)
-    ax3.plot(t_r, r_d, color= 'tab:blue', label= res_label)
-    ax3.legend(bbox_to_anchor =(1,1.1), loc= 'upper right')
-    
-    fig, ax = plt.subplots()
-    ax.set_xlabel('Weg [mm]')
-    ax.set_ylabel('Kraft [N]')
-    ax.plot(s, F, label= 'Messung', color= 'tab:red')
-    ax.plot(s_ref, F_ref, label= 'Referenz: '+ ref_label, color= 'black')
-    ax.legend()
-    mpl.cursor(hover=True)
-    
-    # plt.savefig("res_force_plot.svg", dpi=300.0, orientation='landscape', papertype='a4')
-    
-    plt.show()
-    
-    
-def plot_gradient(t_F, dF, t_r, dr):
-    
-    r_max = []
-    for i in range(len(dr)):
-        if dr[i] == max(dr):
-            r_max.append(i)
-    
-    fig, ax = plt.subplots()
-    ax.set_xlabel('Zeit [s]')
-    ax.set_ylabel('Steigung der Kraft [N/s]', color = 'tab:red')
-    ax.plot(t_F, dF, color= 'tab:red', label= 'Kraftmessung')
-    ax.tick_params(axis='y', labelcolor= 'tab:red')
-    ax.legend(loc='upper left')
-
-    ax2 = ax.twinx()
-    ax2.set_ylabel('Steigung der Widerstands [Ohm/s]', color= 'tab:blue')
-    ax2.plot(t_r[:r_max[0]], dr[:r_max[0]], color= 'tab:blue', label= 'widerstandsmessung')
-    ax2.tick_params(axis='y', labelcolor= 'tab:blue')
-    ax2.legend(loc= 'upper right')
-
 def plot_force_lin_err(x_s, y_force, lin_fit, lin_err):
     fig, ax = plt.subplot_mosaic([[0,1],[2,3]])
     fig.set_tight_layout(True)
+    
     ax[0].set_xlabel('Weg [mm]')
     ax[0].set_ylabel('Kraft [N]')
 
@@ -336,12 +503,11 @@ def plot_force_lin_err(x_s, y_force, lin_fit, lin_err):
     ax[1].legend()
     ax[2].legend()
     ax[3].legend()
-    # mpl.cursor(hover=True)
-    plt.show()
 
 def plot_res_lin_err(x_f, r_value, lin_fit, lin_err):
     fig, ax = plt.subplot_mosaic([[0,1],[2,3]])
     fig.set_tight_layout(True)
+    
     ax[0].set_xlabel('Kraft [N]')
     ax[0].set_ylabel('Widerstand [Ohm]')
 
@@ -373,10 +539,3 @@ def plot_res_lin_err(x_f, r_value, lin_fit, lin_err):
     ax[1].legend()
     ax[2].legend()
     ax[3].legend()
-    mpl.cursor(hover=True)
-    plt.show()
-
-    
-    
-    
-        
--- Shortprogramming.py
+++ Shortprogramming.py
@@ -5,46 +5,128 @@ import PythonLib as pl
 root = pl.tk.Tk()
 root.withdraw()
 
+files = 1
+newsys = False
+
 force, res, ref = [], [], []
+t_r, R, R_d, r, r_d, deltaR = [], [], [], [], [], []
+t_f, f, x = [], [], []
+t_ref, f_ref, x_ref = [], [], []
 
+for i in range(files):
+    force.append(pl.data_transfer_force())
+    res.append(pl.data_transfer_smu())
+    ref.append(pl.data_transfer_ref())
 
-force   = pl.data_transfer_force() 
-res     = pl.data_transfer_smu()
-ref     = pl.data_transfer_ref()
+    t_f.append(pl.export_force_t(force[-1]))
+    f.append(pl.export_force_F(force[-1]))
+    x.append(pl.export_force_s(force[-1]))
 
-t_r     = pl.export_smu_t(res) 
-R       = pl.export_smu_R(res)
-R_d     = (R - R[0])
-r       = R/max(R) * 100
-r_d     = R_d/max(R_d) * 100 
-deltaR = (R - R[0])/R[0] * 100
+    t_ref.append(pl.export_force_t(ref[-1]))
+    f_ref.append(pl.export_force_F(ref[-1]))
+    x_ref.append(pl.export_force_s(ref[-1]))
 
-t_f     = pl.export_force_t(force)
-f       = pl.export_force_F(force)
-x       = pl.export_force_s(force)
+    t_r.append(pl.export_smu_t(res[-1]))
+    R.append(pl.export_smu_R(res[-1]))
+    R_d.append((R[-1] - R[-1][0]))
+    r.append(R[-1]/max(R[-1]) * 100)
+    r_d.append(R_d[-1]/max(R_d[-1]) * 100)
+    deltaR.append((R[-1] - R[-1][0])/R[-1][0] * 100)
 
-t_ref   = pl.export_force_t(ref)
-f_ref   = pl.export_force_F(ref)
-x_ref   = pl.export_force_s(ref)
+y_achse = ['Absolute Differenz zum Anfangswert [Ohm]',
+           'Änderung [%] normiert auf das Maximum',
+           r'$\Delta R/R_0 $ [%]']
+res_label = ['Widerstandsmessung des eingeklemmten Sensors',
+             'Widerstandsmessung der gedruckten Feder',
+             'Widerstand der Zugprobe']
+ref_label = ['Einhausung mit Doppelblechblattfeder',
+             'Einhausung mit Doppel-PLA-Blattfeder']
 
+diff = []
 
+# pl.plot(t_r[0], R[0])
 
-y_achse = ['Absolute Differenz zum Anfangswert [Ohm]', 'Änderung [%] normiert auf das Maximum', 'deltaR/R_0 [%]' ]
-res_label = ['Widerstandsmessung des eingeklemmten Sensors', 'Widerstandsmessung der gedruckten Feder', 'Widerstand der Zugprobe']
-ref_label = ['Einhausung mit Doppelblechblattfeder', 'Einhausung mit Doppel-PLA-Blattfeder']
+for i in range(files):
+    diff.append(pl.compute_time_diff(t_f[i], f[i], t_r[i], deltaR[i]))
+    t_r[i] = t_r[i] + diff[i][0]
+    pl.plot_res_and_force(t_r[i], deltaR[i],
+                          t_f[i], f[i], x[i],
+                          t_ref[i], f_ref[i], x_ref[i],
+                          y_achse[2], res_label[2], ref_label[1])
+    
+# l_0 = 28
+l_0 = 40
 
-diff, f_diff = pl.compute_time_diff(t_f, f, t_r, r, tensile= False)
+strain_res = []
+strain_res_100 = []
+strain_res_max = []
+maxi = []
 
-if diff < 0:
-    t_r = t_r - abs(diff)    
-elif diff > 0:
-    t_r = t_r + abs(diff)
+t_f_temp, F, s = [],[],[]
+t_r_temp, deltaR_temp = [],[]
 
-pl.plot_res_and_force(t_r, deltaR, 
-                      t_f, f, x, 
-                      t_ref, f_ref, x_ref, 
-                      y_achse[2], res_label[2], ref_label[1], sensor= False)
+if newsys == True:
+    for i in range(files):
+        t_f_temp.append(t_f[i][t_f[i]>=50])
+        F.append(f[i][t_f[i]>=50])
+        s.append(x[i][t_f[i]>=50])
+        t_r_temp.append(t_r[i][t_r[i]>=50])
+        deltaR_temp.append(deltaR[i][t_r[i]>=50])
+else:
+    t_f_temp = t_f
+    F = f
+    s = x
+    t_r_temp = t_r
+    deltaR_temp = deltaR
 
-# grad = pl.compute_gradient(t_f, f, t_r, r)
+for i in range(files):
+    strain_res.append(pl.compute_strain_res(l_0,
+                                            t_r_temp[i], deltaR_temp[i], 
+                                            t_f_temp[i], F[i], s[i]))  
+    pl.plot_strain_res(strain_res[i][0], 
+                        strain_res[i][1],
+                        strain_res[i][2], 
+                        strain_res[i][3],
+                        strain_res[i][4])
+index = []
 
-# pl.plot_gradient(t_f, grad[0][0], t_r, grad[1][0])
+for j in range(files):
+    for i in range(len(strain_res[j][0])):
+        if strain_res[j][0][i] == max(strain_res[j][0]):
+            index.append(i)
+            break
+        
+for i in range(files):
+    pl.plot_strain_res(strain_res[i][0][0:index[0]], 
+                        strain_res[i][1][0:index[0]],
+                        strain_res[i][2][0:index[0]], 
+                        strain_res[i][3],
+                        strain_res[i][4])
+    maxi.append(pl.compute_res_max_strain(strain_res[i][0][0:index[0]],
+                                          strain_res[i][1][0:index[0]],
+                                          strain_res[i][2][0:index[0]]))
+    strain_res_max.append(pl.compute_strain_res_rmax(maxi[i][2],
+                                                     maxi[i][3],
+                                                     maxi[i][4]))
+    strain_res_100.append(pl.compute_strain_res_100(strain_res[i][0][0:index[0]], 
+                                                    strain_res[i][1][0:index[0]],
+                                                    strain_res[i][2][0:index[0]]))
+
+# for i in range(files):
+#     pl.plot_strain_res(strain_res_100[i][0],
+#                         strain_res_100[i][1],
+#                         strain_res_100[i][2], 
+#                         strain_res_100[i][3], 
+#                         strain_res_100[i][4])
+
+# for i in range(files):
+#     pl.plot_strain_res(strain_res_max[i][0],
+#                         strain_res_max[i][1],
+#                         strain_res_max[i][2], 
+#                         strain_res_max[i][3], 
+#                         strain_res_max[i][4])
+
+# bible = pl.data_transfer_bible()
+# bible_mean = pl.data_transfer_bible()
+
+# pl.plot_bible(bible, bible_mean)
